d91ad3e6351dae5aa0b42e0f1e8d3eaf0ee65bb7
wangchengwei0122 <892630kux@gmail.com>
2025-07-09 23:24:48 +0800

    chore: add aave address book

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,31 @@
+'use client';
+
 import { AaveV3Sepolia } from '@bgd-labs/aave-address-book';
+import { IUiPoolDataProvider_ABI } from '@bgd-labs/aave-address-book/abis';
+import { useReadContract } from 'wagmi';
 
 export default function Dashboard() {
-  console.log(AaveV3Sepolia);
+  const { data: reservesData } = useReadContract({
+    address: AaveV3Sepolia.UI_POOL_DATA_PROVIDER,
+    abi: IUiPoolDataProvider_ABI,
+    functionName: 'getReservesData',
+    args: [AaveV3Sepolia.POOL_ADDRESSES_PROVIDER],
+  });
+  console.log('reservesData');
+  console.log(reservesData);
+
+  const { data: reservesList } = useReadContract({
+    address: AaveV3Sepolia.UI_POOL_DATA_PROVIDER,
+    abi: IUiPoolDataProvider_ABI,
+    functionName: 'getReservesList',
+    args: [AaveV3Sepolia.POOL_ADDRESSES_PROVIDER],
+  });
+  console.log('reservesList');
+  console.log(reservesList);
+
+  // console.log(AaveV3Sepolia);
+
+  // console.log(IUiPoolDataProviderV3);
+  // console.log(IPoolDataProvider);
   return <div>Dashboard</div>;
 }

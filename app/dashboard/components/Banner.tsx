8d7c19aff2bb878676738e0c0ed49aeafd3fa220
wangchengwei0122 <892630kux@gmail.com>
2025-07-25 22:49:22 +0800

    feat: Add Market component

--- app/dashboard/components/Banner.tsx
+++ app/dashboard/components/Banner.tsx
@@ -1,7 +1,11 @@
+import Market from './Market';
+
 export default function Banner() {
   return (
     <div className="w-full h-60 bg-gray-800 ">
-      <div className="w-full h-full bg-gray-800 "></div>
+      <div className="w-full h-full bg-gray-800 ">
+        <Market />
+      </div>
     </div>
   );
 }

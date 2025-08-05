633eeb446658a23d0f94abde8062f06b843e31dc
wangchengwei <892630kux@gmail.com>
2025-08-05 22:10:22 +0800

    styles: markets

--- app/markets/page.tsx
+++ app/markets/page.tsx
@@ -3,6 +3,7 @@ import Assets from './components/Assets';
 export default function Markets() {
   return (
     <div className="min-h-screen bg-[#f1f1f3]">
+      <div></div>
       <Assets />
     </div>
   );

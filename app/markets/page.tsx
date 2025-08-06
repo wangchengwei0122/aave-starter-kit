bd1a11113727879cb58691a6f616f9c3a800fa2f
wangchengwei <892630kux@gmail.com>
2025-08-06 23:54:12 +0800

    chore: Markest Component

--- app/markets/page.tsx
+++ app/markets/page.tsx
@@ -1,10 +1,13 @@
 import Assets from './components/Assets';
+import Head from './components/head';
 
 export default function Markets() {
   return (
-    <div className="min-h-screen bg-[#f1f1f3]">
-      <div></div>
-      <Assets />
-    </div>
+    <>
+      <Head />
+      <div className="min-h-screen bg-[#f1f1f3]">
+        <Assets />
+      </div>
+    </>
   );
 }

bb2c0483c09abe2cb3b1d422286a52eaa18595ab
wangchengwei <892630kux@gmail.com>
2026-02-01 14:41:42 +0800

    feat(web): convert submodule to monorepo app

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@workspace/ui/components/button";
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Button variant="outline">Button</Button>
+      <Button variant="outline" size="icon" aria-label="Submit">
+       1112
+      </Button>
+    </div>
+  );
+}
